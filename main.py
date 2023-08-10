d7b9f42907629b139c0855dd874469613ffc4d3a
ctorrington <ctorrington1@gmail.com>
2023-08-10 16:31:59 +0100

    Added state actions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 All episodes begin in the center.
 Move either left or right by one state, all with equal probability.
 (in the project, states are aggregated,
@@ -11,3 +12,49 @@ In the case that the state is near an edge,
 Termination on the left produces a reward of -1.
 Termination on the right produces a reward of +1.
 All other transitions have a reward of 0.
+"""
+
+from constants import Constants
+
+import math
+
+# Constants
+ACTIONS = Constants.ACTIONS
+
+class ThousandStateRandomWalk:
+    """Random walk environment."""
+
+    def __init__(self) -> None:
+        """Initialse the environment."""
+
+        # State space values.
+        self.number_of_states = 1000
+        self.number_of_aggregated_states = 10
+        self.states_per_block = (
+            self.number_of_states / self.number_of_aggregated_states
+        )
+        self.starting_state = math.floor(self.number_of_states / 2)
+        self.current_state = self.starting_state
+
+        self.current_block = math.floor(self.current_state / (self.states_per_block))
+        
+        # State space set up.
+        self.state_space = {}
+        for state in range(self.number_of_states):
+            # Values for each state.
+            self.state_space[state] = {
+                "actions": {},
+                "policy": {},
+                "estimated return": 0,
+                "state entries": 0,
+            }
+
+            # Actions for each state.
+            for action in ACTIONS:
+                self.state_space[state]["actions"][action] = {
+                    "value": 0,
+                    "cumulative weight": 0,
+                }
+
+            # Policy.
+            
