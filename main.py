f4d226a689f570c5ffd9bc61bb19ae2453ee2863
Christopher Torrington <ctorrington1@gmail.com>
2023-08-10 14:56:19 +0100

    project explanation

---
+++ main.py
@@ -0,0 +1,13 @@
+All episodes begin in the center.
+Move either left or right by one state, all with equal probability.
+(in the project, states are aggregated,
+ so there are 100 states to the left & 100 states to the right.)
+In the case that the state is near an edge,
+ then the probability of that would have gone into the missing neighbours
+ goes into the probability of terminating on that side.
+(thus, state 1 has a 0.5 chance of terminating on the left,
+ & state 950 has a 0.25 chance of terminating on the right)
+
+Termination on the left produces a reward of -1.
+Termination on the right produces a reward of +1.
+All other transitions have a reward of 0.
