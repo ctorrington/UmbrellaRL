63df91d1814833f393d70cae0a8d3e78ab8746c0
ctorrington <ctorrington1@gmail.com>
2023-08-26 16:21:00 +0100

    random walk attributes

--- random_walk.py
+++ random_walk.py
@@ -5,7 +5,10 @@ Random walk configuration.
 class RandomWalk:
     def __init__(self) -> None:
         self.number_of_episodes = 100000
+        self.estimated_value = 0
         self.alpha = 0.00002
+        self.dimensionality = 1000
+        self.weights = [0 for i in range(self.dimensionality)]
 
         for episode_number in range(self.number_of_episodes):
             self.generate_episode()
@@ -13,3 +16,4 @@ class RandomWalk:
 
     def generate_episode(self):
         """Single episode for the random walk."""
+
