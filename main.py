b1e23db4e21bff686df293f68b64003b31905ee1
ctorrington <ctorrington1@gmail.com>
2023-08-27 18:18:55 +0100

    Fixed state space reward for right-side state

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class ThousandStateRandomWalk:
 
         self.current_block = math.floor(self.current_state /
                                          self.states_per_block)
-        
+
         # State space set up.
         self.state_space = {}
         for state in range(0, self.number_of_states - 1):
@@ -71,7 +71,7 @@ class ThousandStateRandomWalk:
 
         # Terminal rewards.
         self.state_space[0]["reward"] = -1
-        self.state_space[self.number_of_states - 1] = 1
+        self.state_space[self.number_of_states - 1]["reward"] = 1
 
     def set_current_block(self) -> None:
         """Updates the current block that the agent is currently in."""
