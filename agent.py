c8cc94b1cead7f85b20cc54f816776ee62c45d87
ctorrington1 <ctorrington1@gmail.com>
2023-10-10 14:30:35 +0100

    agent loop for state value function

--- agent.py
+++ agent.py
@@ -23,12 +23,24 @@ class Agent:
     def determine_value_function_over_state_space(self) -> None:
         """Determine the value of all states across the state space."""
         
-        pass
-    
-    def set_state_value(self, state) -> None:
+        terminal_states = self.get_terminal_states()
+        
+        # Update the estimated returns for all states in the state space until
+        # a theta is reached (theta is the value used for accuracy).
+        while True:
+            delta = 0
+            
+            # Update the expected return for each state.
+            for state in self.environment.get_state_space():
+                if state not in self.environment.get_terminal_states():
+                    previous_state_return = self.environment.get_state_return(state)
+                    self.environment.set_state_return(state, self.determine_state_value(state))
+            
+    def determine_state_value(self, state: int) -> int | float:
         """
-        Update the estimated return for the given state.
+        Determine the estimated return for the given state.
         This method uses the state value function.
         """
-        pass
+        
+        
         
