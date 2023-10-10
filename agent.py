cca9c4ab0f587ae228d5b9a85781c5827e76eed3
ctorrington1 <ctorrington1@gmail.com>
2023-10-10 14:52:29 +0100

    agent action distribution

--- agent.py
+++ agent.py
@@ -19,6 +19,20 @@ class Agent:
         """Get the terminal states of the state space"""
         
         return self.environment.get_terminal_states()
+    
+    def get_action_probability_distribution(self, state: int) -> dict[ACTIONS, int | float]:
+        """Return the probability distribution for the actions of the given state."""
+        
+        action_probability_distribution = {}
+        
+        # Random policy.
+        if self.policy == "random":
+            action_probability = 1 / (len(self.environment.get_possible_actions(state)))
+            
+            for action in self.environment.get_possible_actions(state):
+                action_probability_distribution[action] = action_probability
+                
+        return action_probability_distribution
 
     def determine_value_function_over_state_space(self) -> None:
         """Determine the value of all states across the state space."""
@@ -42,5 +56,6 @@ class Agent:
         This method uses the state value function.
         """
         
-        
+        state_return = self.environment.get_state_return(state)
+        possible_actions = self.environment.get_possible_actions(state)
         
