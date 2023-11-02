b3e206c155a5b8fc29c0bf642dc3983736b8dce7
ctorrington1 <ctorrington1@gmail.com>
2023-11-02 16:50:38 +0000

    StateSpace inherits from dictionary

--- StateSpace.py
+++ StateSpace.py
@@ -7,20 +7,27 @@ then the state space should be injected into the environment.
 
 from State import State
 
-class StateSpace:
-    """Environment State Space Dependency."""
+class StateSpace(dict):
+    """Environmnet State Space dependency."""
     
     def __init__(self, number_of_states: int,
-                 terminal_state_rewards: dict[int, int]) -> None:
-        self.state_space: dict[int, State] = {}
+                 terminal_states_rewards: dict[int, int]) -> None:
         for state in range(number_of_states):
-            self.state_space[state] = State()
+            self[state] = State()
             
         # Terminal states.
-        for state in terminal_state_rewards:
-            self.state_space[state].is_terminal = True
-        
+        for state in terminal_states_rewards:
+            self[state].is_terminal = True
+            
         # Terminal state rewards.
-        self.state_space[0].reward = -1
-        self.state_space[number_of_states - 1].reward = 1
+        self[0].reward = -1
+        self[number_of_states - 1].reward = 1
         
+    def __getattr__(self, key):
+        if key in self:
+            return self[key]
+        else:
+            raise AttributeError(f"'StateSpace' object has not attribute '{key}'")
+ 
+    def __setattr__(self, key, value):
+        self[key] = value
