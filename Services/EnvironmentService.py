c7dc332f9b3b4c12c46dc61295728e07674e5f1e
ctorrington <ctorrington1@gmail.com>
2023-11-04 08:47:56 +0000

    get state transition probability

--- Services/EnvironmentService.py
+++ Services/EnvironmentService.py
@@ -1,7 +1,9 @@
 """Environment Service."""
 
+from argparse import Action
 from StateSpace import StateSpace
 from constants import Constants
+from State import State
 
 ACTIONS = Constants.ACTIONS
 
@@ -21,4 +23,18 @@ class EnvironmentService:
                 return [current_state - 1]
             case ACTIONS.RIGHT:
                 return [current_state + 1]
+            
+    def get_state_transition_probability(self, current_state_index: int,
+                                         action: ACTIONS,
+                                         next_state_index: int):
+        """
+        Return the probability of transitioning from one state to another
+        given an action.
+        """
+       
+        # Catch states transitioning outside of the state space.
+        if next_state_index < 0 or next_state_index > len(self.state_space):
+            raise ValueError("Invalid index for next state. The index must be within the range of the State Space.")
+ 
+        return 1
             
