592561206a0d0cf7b13c356adb6683bea0a95b30
ctorrington <ctorrington1@gmail.com>
2023-11-04 12:29:10 +0000

    Dependency injection for StateSpace & ActionProbability

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,10 +1,28 @@
 """RL Agent."""
 
+from ActionProbability import ActionProbability
+from StateSpace import StateSpace
+
 class Agent:
     """RL Agent."""
     
     def __init__(self):
-        pass
+        number_of_states = 1000
+        state_rewards = {0: -1, number_of_states: 1}
+        
+        self.state_space: StateSpace = StateSpace(
+            number_of_states,
+            state_rewards
+        )
+        self.action_probability: ActionProbability = ActionProbability(
+            self.state_space
+        )
     
-    def interact_with_environment(self):
+    def learn(self):
+        self.interact_with_environment()
+        
+    def interact_with_environment(self) -> None:
+        self.evaluate_policy()
+        
+    def evaluate_policy(self) -> None:
         pass
