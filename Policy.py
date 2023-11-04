f14d6b132c6dcafd0e464aa6e48128aa9dee7441
ctorrington <ctorrington1@gmail.com>
2023-11-04 20:21:28 +0000

    added choose action method

--- Policy.py
+++ Policy.py
@@ -3,9 +3,23 @@ Policy class used for managing action selection for a reinforcement
 learning agent.
 """
 
+import random
+
 from ActionProbabilityDistribution import ActionProbabilityDistribution
+from constants import Constants
+
+ACTIONS = Constants.ACTIONS
 
 class Policy:
     def __init__(self, action_probability_distribution: ActionProbabilityDistribution):
         self.action_probability_distribution = action_probability_distribution
+        
+    def choose_action(self, state: int) -> ACTIONS:
+        """Choose an action based on the Action Probability Distribution."""
+        
+        state_distribution: dict[ACTIONS, float] = self.action_probability_distribution[state]
+        
+        action_chosen: ACTIONS = random.choice(list(state_distribution.keys()))
+        
+        return action_chosen
         
