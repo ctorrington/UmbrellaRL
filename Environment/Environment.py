e2edd7d07725be210754653ef85db50f1433837f
ctorrington <ctorrington1@gmail.com>
2023-11-04 09:06:13 +0000

    get state transition probability

--- Environment/Environment.py
+++ Environment/Environment.py
@@ -44,3 +44,17 @@ class Environment:
     def get_next_states(self, current_state: int,
                         action: ACTIONS) -> list[int]:
         return self.environment_service.get_next_states(current_state, action)
+    
+    def get_state_transition_probability(self, current_state_index: int,
+                                         action: ACTIONS,
+                                         next_state_index: int) -> float:
+        """
+        Return the probability of transitioning from one state to another
+        given an action.
+        """
+        
+        return self.environment_service.get_state_transition_probability(
+            current_state_index,
+            action,
+            next_state_index
+        )
