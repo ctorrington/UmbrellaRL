ef560b055dd77b91bf8c0928086a3f63c17a0a07
ctorrington1 <ctorrington1@gmail.com>
2023-11-07 10:02:20 +0000

    Enhanced Action Probability Distribution

--- ActionProbabilityDistribution.py
+++ ActionProbabilityDistribution.py
@@ -4,24 +4,10 @@ learning agents.
 """
 
 from constants import Constants
-from StateSpace import StateSpace
 
 ACTIONS = Constants.ACTIONS
 
-class ActionProbabilityDistribution(dict[int, dict[ACTIONS, float]]):
-    """
-    Class for representing the action probability distribution for reinforcement
-    learning agents.
-    """
-    
-    def __init__(self, state_space: StateSpace) -> None:
-        """Action probability is initialised to equiprobable."""
-        
-        for state in state_space:
-            self[state] = {action: 1/ len(ACTIONS.as_tuple()) for action in ACTIONS.as_tuple()}
-
-
-class ActionProbabilityDistribution2(dict[ACTIONS, float]):
+class ActionProbabilityDistribution(dict[ACTIONS, float]):
     """
     Class for representing the action probability distribution of each state.
     
