417fa564bc6c03d74637fa0d4fd4985b85cfb0a3
ctorrington1 <ctorrington1@gmail.com>
2023-11-09 11:59:45 +0000

    State Actions class with Actions initialised

--- GridWorld/StateActions.py
+++ GridWorld/StateActions.py
@@ -1,3 +1,18 @@
-"""Grid World Actions for each State."""
+"""
+Grid World Actions for each State.
 
-class GridWorldStateActions:
+Mapping from each State to each action available to them.
+"""
+
+from typing import Dict, List
+
+from Action import Action
+
+class GridWorldStateActions(Dict[int, List[Action]]):
+    """Dictionary object representing the Actions available to each State."""
+    
+    def __init__(self,
+                 number_of_actions: int
+                ):
+        for state in range(number_of_actions):
+            self[state] = Action.members()
