67f53123e7997c2c649c1e9201a4067de01cf036
ctorrington1 <ctorrington1@gmail.com>
2023-11-25 15:58:26 +0000

    changed to accept generic Action types

--- State.py
+++ State.py
@@ -2,11 +2,15 @@
 
 from Action import Action
 
+from typing import List, TypeVar
+
+T = TypeVar('T', bound = Action)
+
 class State:
     """States representation."""
     
     def __init__(self):
-        self.actions: list[Action] = []
+        self.actions: List[T] = []
         self.estimated_return: float = 0
         self.counter: int = 0
         self.reward: int = 0
