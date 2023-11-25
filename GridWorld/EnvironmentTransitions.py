b161e59a2c61ea1049c14160ece0fcee2f5ccefc
ctorrington1 <ctorrington1@gmail.com>
2023-11-25 15:59:28 +0000

    init

--- GridWorld/EnvironmentTransitions.py
+++ GridWorld/EnvironmentTransitions.py
@@ -1,3 +1,21 @@
 """Grid World Environment Transitions.
 
 Dictionary representing the next states following an action in a state."""
+
+class GridWorldEnvironmentTransitions(dict[int, dict[int, float]]):
+    """
+    Mapping data structure for the probabilities for getting from one state
+    to another.
+    
+    0    1   2   3
+    4    5   6   7
+    8    9  10  11
+    12  13  14  15
+    
+    """
+    
+    def __init__(self,
+                 number_of_states:int
+                ) -> None:
+        
+        
