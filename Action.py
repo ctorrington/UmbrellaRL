474ab103e46643171eedc02f405002de85839c8b
ctorrington1 <ctorrington1@gmail.com>
2023-11-25 15:57:40 +0000

    changed to generic class

--- Action.py
+++ Action.py
@@ -5,14 +5,16 @@ Actions that can be implemented by a reinforcement learning agent.
 """
 
 from enum import Enum
-from typing import List
+from typing import List, TypeVar
+
+T = TypeVar('T', bound = 'Action')
 
 class Action(Enum):
     """Generic Action class."""
     pass
     
     @classmethod
-    def members(cls) -> List['Action']:
+    def members(cls: T) -> List[T]:
         """Return all actions available."""
         
         return list(cls)
