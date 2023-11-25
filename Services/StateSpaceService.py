db7bee3460cc41e14aed4d1e83c15c6f118257f3
Christopher Torrington <ctorrington1@gmail.com>
2023-11-25 21:11:57 +0000

    temp fix circular imports

--- Services/StateSpaceService.py
+++ Services/StateSpaceService.py
@@ -2,15 +2,31 @@
 
 # TODO Actually do this Service class.
 
-from StateSpace import StateSpace
+from typing import Dict, List
+
+from Action import Action
+from State import State # TODO temp fix for ciruclar imports. 
+                        # Would prefer StateSpace type to Dict[int, State]
 
 class StateSpaceService:
     """Service class for the StateSpace."""
     
-    def set_rewards(self, state_space: StateSpace,
-                    rewards: dict[int, int]) -> None:
-        """Set the rewards for the State Space."""
+    # TODO Custom states need to be considered. ie, custom terminal properties.
+    @staticmethod
+    def set_terminal_states_rewards(state_space: Dict[int, State],
+                                    terminal_states_rewards: Dict[int, float]
+                                   ) -> None:
+        """Set the rewards & properties for the terminal States."""
         
-        for state in rewards:
-            state_space[state].reward = rewards[state]
+        for state in terminal_states_rewards:
+            state_space[state].reward = terminal_states_rewards[state]
+            state_space[state].is_terminal = True
             
+    @staticmethod
+    def set_state_actions(state_space: Dict[int, State],
+                          state_actions: Dict[int, List[Action]]
+                         ) -> None:
+        """Set the actions for each State."""
+        
+        for state in state_actions:
+            state_space[state].actions = state_actions[state]
