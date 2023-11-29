e7886485072efa1e943cc082cb0a573bbef45886
Christopher Torrington <ctorrington1@gmail.com>
2023-11-29 20:01:10 +0000

    marked for removal

--- Services/EnvironmentService.py
+++ Services/EnvironmentService.py
@@ -1,39 +1,41 @@
-"""Environment Service."""
+# """Environment Service."""
 
-from StateSpace import StateSpace
-from Action import Action
+# TODO MARKED FOR REMOVAL.
 
-class EnvironmentService:
-    """Environment Service."""
+# from StateSpace import StateSpace
+# from Action import Action
+
+# class EnvironmentService:
+#     """Environment Service."""
     
-    def __init__(self, state_space: StateSpace) -> None:
-        self.state_space = state_space
+#     def __init__(self, state_space: StateSpace) -> None:
+#         self.state_space = state_space
         
-    def get_next_states(self,
-                        current_state: int,
-                        action: Action
-                       ) -> list[int]:
-        """Return the next states from the current state following an action."""
+#     def get_next_states(self,
+#                         current_state: int,
+#                         action: Action
+#                        ) -> list[int]:
+#         """Return the next states from the current state following an action."""
         
-        match action:
-            case Action.left:
-                return [current_state - 1]
-            case Action.right:
-                return [current_state + 1]
+#         match action:
+#             case Action.left:
+#                 return [current_state - 1]
+#             case Action.right:
+#                 return [current_state + 1]
             
-    def get_state_transition_probability(self,
-                                         current_state_index: int,
-                                         action: Action,
-                                         next_state_index: int
-                                        ) -> float:
-        """
-        Return the probability of transitioning from one state to another
-        given an action.
-        """
+#     def get_state_transition_probability(self,
+#                                          current_state_index: int,
+#                                          action: Action,
+#                                          next_state_index: int
+#                                         ) -> float:
+#         """
+#         Return the probability of transitioning from one state to another
+#         given an action.
+#         """
        
-        # Catch states transitioning outside of the state space.
-        if next_state_index < 0 or next_state_index > len(self.state_space):
-            raise ValueError("Invalid index for next state. The index must be within the range of the State Space.")
+#         # Catch states transitioning outside of the state space.
+#         if next_state_index < 0 or next_state_index > len(self.state_space):
+#             raise ValueError("Invalid index for next state. The index must be within the range of the State Space.")
  
-        return 1
+#         return 1
             
