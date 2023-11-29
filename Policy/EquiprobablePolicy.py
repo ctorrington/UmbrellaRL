62f06cca3a8400c06b146cfe247f130156e29c8a
Christopher Torrington <ctorrington1@gmail.com>
2023-11-29 19:24:11 +0000

    temp removed. provided in later version

--- Policy/EquiprobablePolicy.py
+++ Policy/EquiprobablePolicy.py
@@ -3,30 +3,32 @@ Policy class used for managing action selection for a reinforcement
 learning agent.
 """
 
-import random
+# TODO This Policy will be provided eventually as a builtin.
 
-from ActionProbabilityDistribution import ActionProbabilityDistribution
-from Policy.BasePolicy import BasePolicy
-from StateSpace import StateSpace
-from Action import Action
+# import random
 
-class EquiprobablePolicy(BasePolicy):
-    def __init__(self,
-                 state_space: StateSpace
-                ) -> None:
-        super().__init__(state_space)
+# from ActionProbabilityDistribution import ActionProbabilityDistribution
+# from Policy.BasePolicy import BasePolicy
+# from StateSpace import StateSpace
+# from Action import Action
+
+# class EquiprobablePolicy(BasePolicy):
+#     def __init__(self,
+#                  state_space: StateSpace
+#                 ) -> None:
+#         super().__init__(state_space)
         
-        action_probability: float = 1/ len(Action.members())
+#         action_probability: float = 1/ len(Action.members())
         
-        for state in state_space:
-            for action in state_space[state].actions:
-                self[state][action] = action_probability
+#         for state in state_space:
+#             for action in state_space[state].actions:
+#                 self[state][action] = action_probability
             
-    def choose_action(self, state: int) -> Action:
-        """Choose an action based on the Action Probability Distribution."""
+#     def choose_action(self, state: int) -> Action:
+#         """Choose an action based on the Action Probability Distribution."""
 
-        return random.choice(list(self[state].keys()))
+#         return random.choice(list(self[state].keys()))
 
-    def get_action_probability_distribution(self, state: int) -> ActionProbabilityDistribution:
+#     def get_action_probability_distribution(self, state: int) -> ActionProbabilityDistribution:
 
-        return self[state]
+#         return self[state]
