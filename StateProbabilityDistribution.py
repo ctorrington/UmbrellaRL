dae7d33ec21b6d280af59ec6cf5600a3cc2cb854
ctorrington <ctorrington1@gmail.com>
2023-11-30 11:05:41 +0000

    __getitem__ override

--- StateProbabilityDistribution.py
+++ StateProbabilityDistribution.py
@@ -1,7 +1,7 @@
 """
 State Probability Distribution type.
 
-Mapping from States potentially resulting, from an Action, to the percentage 
+Mapping from States potentially resulting from an Action to the percentage 
 chance of the State occuring, according to the Environment.
 
 Used by the Environment.
@@ -12,5 +12,11 @@ from typing import Dict
 
 from StateIndex import StateIndex # type: ignore
 
+# TODO this class needs to implement __getsetetcitem__ methods for classes.
+    # The getsetetc methods need to check if a State exists, otherwise
+    # return a custom Attribute error for that State not existing - then
+    # thats handled or equated to 0.
 class StateProbabilityDistribution[StateIndex](ABC, Dict[StateIndex, float]):
-    pass
+    def __getitem__(self, key: StateIndex) -> float:
+        return self[key]
+    
