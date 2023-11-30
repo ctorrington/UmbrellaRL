7304565bad920bc55369e41376e61584138c419e
ctorrington <ctorrington1@gmail.com>
2023-11-30 11:30:07 +0000

    cleaning up

--- Environment/Environment.py
+++ Environment/Environment.py
@@ -27,23 +27,23 @@ class Environment[StateIndex](ABC, Dict[StateIndex, StateActions[StateIndex]]):
     def __init__(self):
         # Environment properties.
         self.number_of_states: int = len(self)
-        
+
     def get_next_states(self,
                         current_state: StateIndex,
                         action: Action
                        ) -> StateProbabilityDistribution[StateIndex]:
         return self[current_state][action]
-    
+
     # TODO Potentially redundant given the get-next-states method returning 
         # the distribution.
     def get_state_transition_probability(self,
-                                         current_state_index: StateIndex,
+                                         current_state: StateIndex,
                                          action: Action,
-                                         next_state_index: StateIndex
+                                         next_state: StateIndex
                                         ) -> float:
         """
         Return the probability of transitioning from one state to another
         given an action.
         """
  
-        return self[current_state_index][action][next_state_index]       
+        return self[current_state][action][next_state]
