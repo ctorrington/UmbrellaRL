4d3badde8ccc1a64e7e6c75f0229186391130d87
Christopher Torrington <ctorrington1@gmail.com>
2023-12-03 15:34:09 +0000

    added optional types for coordinates

--- GridWorld/State.py
+++ GridWorld/State.py
@@ -1,10 +1,12 @@
 """Grid World representation of a State."""
 
+from typing import Optional
+
 from State import State
 
 class GridWorldState(State):
-    def __init__(self):
+    def __init__(self, x: Optional[int] = None, y: Optional[int] = None):
         super().__init__()
         
-        self.x: int
-        self.y: int
+        self.x: int = x
+        self.y: int = y
