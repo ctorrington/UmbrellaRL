e5b5cb0cc499484a264e86f764c6363fc7d26685
Christopher Torrington <ctorrington1@gmail.com>
2023-12-03 15:24:18 +0000

    State Space

--- GridWorld/StateSpace.py
+++ GridWorld/StateSpace.py
@@ -1,19 +1,10 @@
 """State Space for the Grid World."""
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from StateSpace import StateSpace
 
-from GridWorld.State import GridWorldState
-from GridWorld.Action import GridWorldAction
+index_type = Tuple[int, int]
 
-class GridWorldStateSpace(StateSpace):
-    def __init__(self,
-                 number_of_states: int,
-                 terminal_states_rewards: Dict[int, int],
-                 state_actions: Dict[int, List[GridWorldAction]]
-                ):
-        super().__init__(number_of_states,
-                         terminal_states_rewards,
-                         state_actions, # HERE
-                         state_class = GridWorldState)
+class GridWorldStateSpace(StateSpace[index_type]):
+    """Grid World State Space representation."""
