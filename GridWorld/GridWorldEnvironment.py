66a0d45b5bb809d82fdae0f739908e567d382a66
Christopher Torrington <ctorrington1@gmail.com>
2023-12-04 19:58:52 +0000

    added determine next states function

--- GridWorld/GridWorldEnvironment.py
+++ GridWorld/GridWorldEnvironment.py
@@ -1,11 +1,19 @@
 """Grid World Environment."""
 
-from typing import Tuple
+from typing import Tuple, Dict
 
 from Environment.Environment import Environment
 
 from GridWorld.StateSpace import GridWorldStateSpace
 from GridWorld.Action import GridWorldAction
+
+
+# Grid World State Space representation.
+
+# 00 01 02 03
+# 04 05 06 07
+# 08 09 10 11
+# 12 13 14 15
 
 # TODO class to reprsent the Tuple[int, int] type.
 class GridWorldEnvironment(Environment[Tuple[int, int]]):
@@ -29,10 +37,54 @@ class GridWorldEnvironment(Environment[Tuple[int, int]]):
                     # The potential next States are given here, along with their
                     # probabilities - that is what needs to be calculated &
                     # applied to this reference below.
-                self[state][action] = 
+                self[state][action] = self.determine_next_states(state, action)
                 
-                
+    def determine_next_states(self,
+                              state: Tuple[int, int],
+                              action: GridWorldAction
+                             ) -> Dict[Tuple[int, int], float]:
+        """
+        Return a mapping of the possible next States & their probability of
+        occuring.
         
+        In this Grid World, Actions are deterministic.
+        Up will only go up (if possible).
+        
+        If an Action would take the Agent off the State Space, then the next 
+        state is the State the Agent is currently in.
+        """
+        
+        # TODO error checking State is correct type for Grid World problem. 
+        row, column = state
+        
+        possible_next_states: Dict[Tuple[int, int], float] = {}
+        
+        match action:
+            case GridWorldAction.UP:
+                if row > 0:
+                    possible_next_states[row - 1, column] = 1
+                else:
+                    possible_next_states[row, column] = 1
+                    
+            case GridWorldAction.DOWN:
+                if row < self.state_space.number_of_rows - 1:
+                    possible_next_states[row + 1, column] = 1
+                else:
+                    possible_next_states[row, column] = 1
+                
+            case GridWorldAction.LEFT:
+                if column > 0:
+                    possible_next_states[row, column - 1] = 1
+                else:
+                    possible_next_states[row, column] = 1
+                
+            case GridWorldAction.RIGHT:
+                if column < self.state_space.number_of_columns - 1:
+                    possible_next_states[row, column + 1] = 1
+                else:
+                    possible_next_states[row, column] = 1
+                    
+        return possible_next_states
 
 # TODO
     # (This is the Environment itself (nested dict with nested dict))
