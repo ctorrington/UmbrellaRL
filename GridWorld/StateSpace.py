53b95d901c38907aa32b260732a6ec7e7fc4ea2a
Christopher Torrington <ctorrington1@gmail.com>
2023-12-04 19:58:19 +0000

    cleaning up

--- GridWorld/StateSpace.py
+++ GridWorld/StateSpace.py
@@ -9,7 +9,10 @@ from StateSpace import StateSpace
 class GridWorldStateSpace(StateSpace[Tuple[int, int]]):
     """Grid World State Space representation."""
     
-    def __init__(self, number_of_rows: int, number_of_columns: int) -> None:
+    def __init__(self,
+                 number_of_rows: int,
+                 number_of_columns: int
+                ) -> None:
         self.number_of_rows = number_of_rows
         self.number_of_columns = number_of_columns
         
