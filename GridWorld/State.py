433d1f7ed9f3a5cfc54aeedd18511701b6c3acc1
Christopher Torrington <christopher.torrington@xandertalent.com>
2023-12-05 17:17:28 +0000

    allows for State generic type

--- GridWorld/State.py
+++ GridWorld/State.py
@@ -3,8 +3,9 @@
 from typing import Optional
 
 from State import State
+from GridWorld.Action import GridWorldAction
 
-class GridWorldState(State):
+class GridWorldState(State[GridWorldAction]):
     def __init__(self, x: Optional[int] = None, y: Optional[int] = None):
         super().__init__()
         
