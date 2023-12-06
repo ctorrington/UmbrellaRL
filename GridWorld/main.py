c407bed03ebd594cc39eb08df6cea978b8f902da
Christopher Torrington <ctorrington1@gmail.com>
2023-12-06 10:33:04 +0000

    Grid World implementation

--- GridWorld/main.py
+++ GridWorld/main.py
@@ -1,12 +1,38 @@
-from GridWorld.StateActions import GridWorldStateActions
+from typing import Tuple
+
+from Agent.Agent import Agent
+
+from GridWorld.StateSpace import GridWorldStateSpace
+from GridWorld.Action import GridWorldAction
+
+from GridWorld.GridWorldEnvironment import GridWorldEnvironment
+
+from GridWorld.Policy import GridWorldEquiprobablePolicy
 
 def main():
     
-    number_of_states: int = 16
+    number_of_rows: int = 4
+    number_of_columns: int = 4
     
-    state_actions: GridWorldStateActions = GridWorldStateActions(
-        number_of_states
+    # Agent Dependencies.
+    
+    state_space: GridWorldStateSpace = GridWorldStateSpace(
+        number_of_rows,
+        number_of_columns
     )
+    
+    environment: GridWorldEnvironment = GridWorldEnvironment(state_space)
+    
+    policy: GridWorldEquiprobablePolicy[Tuple[int, int], GridWorldAction] = GridWorldEquiprobablePolicy(state_space)
+    
+    agent: Agent[Tuple[int, int], GridWorldAction] = Agent(
+        environment,
+        state_space,
+        policy
+    )
+    
+    agent.evaluate_policy()
+    
     
 
 if __name__ == "__main__":
