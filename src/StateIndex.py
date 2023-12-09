bdd7811c8505dd100ef283ecb66279ee020f97fb
Christopher Torrington <ctorrington1@gmail.com>
2023-12-09 08:28:13 +0000

    todo

--- src/StateIndex.py
+++ src/StateIndex.py
@@ -11,5 +11,7 @@ Currently allowed types:
 
 from typing import Tuple, Any
 
+# TODO probably want to use something like this: https://docs.python.org/3/library/typing.html#user-defined-generic-types:~:text=type%20Response%5BS%5D%20%3D%20Iterable%5BS%5D%20%7C%20int
+
 class StateIndex[T: int | str | Tuple[Any, ...]]:
     pass
