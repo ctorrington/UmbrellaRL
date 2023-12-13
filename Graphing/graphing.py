be0c8a0ac836224c783f35aa3ffcda0728e807d7
Christopher Torrington <ctorrington1@gmail.com>
2023-12-13 17:49:18 +0000

    add init

--- Graphing/graphing.py
+++ Graphing/graphing.py
@@ -2,5 +2,18 @@
 
 import matplotlib as mlp
 
-class Graphing():
+from src.StateIndex import StateIndex
+from src.Action import Action
+from src.Agent.Agent import Agent
+
+class Graphing[StateIndex, A: Action]():
     """Graphing class for UmbrellaRL package."""
+    
+    def __init__(self,
+                 agent: Agent[StateIndex, A]
+                ) -> None:
+        
+        # Dependencies.
+        self.agent = agent
+
+    # TODO overarching method that prints some dashboard with lots of lots.
