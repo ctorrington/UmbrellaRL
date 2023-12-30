6a3e78a6f944a5ff86a58cc2a97aadf650f62955
Christopher Torrington <ctorrington1@gmail.com>
2023-12-30 11:03:54 +0000

    add History class to Agent

---
+++ src/Agent/History.py
@@ -0,0 +1,22 @@
+"""
+Agent History Class.
+
+Currently trackable metrics:
+    - Actions
+"""
+
+from typing import Dict, List
+
+from src.Action import Action
+
+class History[A: Action](Dict[int, Dict[str, List[A]]]):
+    """
+    Agent History class for recording the history of the Environment & 
+    State Value Function during Action interactions.
+    """
+    
+    def __init__(self) -> None:
+        
+        self.count = 0
+        
+        self[0] = {}
