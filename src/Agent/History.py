8ef9b8d859fd77d4f3625a8f85e193bc54cfb2bb
Christopher Torrington <ctorrington1@gmail.com>
2023-12-30 11:09:39 +0000

    add track state value function method

--- src/Agent/History.py
+++ src/Agent/History.py
@@ -6,10 +6,14 @@ Currently trackable metrics:
 """
 
 from typing import Dict, List
+from copy import deepcopy
+from numpy import float64
+
+import numpy.typing as npt
 
 from src.Action import Action
 
-class History[A: Action](Dict[int, Dict[str, List[A]]]):
+class History[A: Action](Dict[int, Dict[str, List[A] | npt.NDArray[float64]]]):
     """
     Agent History class for recording the history of the Environment & 
     State Value Function during Action interactions.
@@ -25,6 +29,14 @@ class History[A: Action](Dict[int, Dict[str, List[A]]]):
         self,
         actions: List[A]
     ) -> None:
-        """Record the given actions."""
+        """Record the given Actions."""
         
-        self[self.count]["actions"] = actions
+        self[self.count]["actions"] = deepcopy(actions)
+        
+    def track_state_value_function(
+        self,
+        state_value_function: npt.NDArray[float64]
+    ) -> None:
+        """Record the given State Value Function."""
+        
+        self[self.count]["state value function"] = deepcopy(state_value_function)
