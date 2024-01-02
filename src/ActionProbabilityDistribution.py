066f9f78e903d7dcaeea69eea9fb807241ffbee9
Christopher Torrington <ctorrington1@gmail.com>
2024-01-02 20:04:30 +0000

    clean up

--- src/ActionProbabilityDistribution.py
+++ src/ActionProbabilityDistribution.py
@@ -5,19 +5,24 @@ Mapping from Actions available to a State to the percentage chance they are
 chosen by the Policy.
 """
 
+from typing import Dict
+
 from src.Action import Action
 
-class ActionProbabilityDistribution[A: Action](dict[A, float]):
-    def __getitem__(self,
-                    key: A
-                   ) -> float:
+class ActionProbabilityDistribution[A: Action](Dict[A, float]):
+
+    def __getitem__(
+        self,
+        key: A
+        ) -> float:
+
         return self[key]
-    
-    def get_action_probability(self,
-                               action: A
-                              ) -> float:
+
+    def get_action_probability(
+        self,
+        action: A
+        ) -> float:
+
         """Return the probability of the given action being chosen."""
-        
-        # TODO potential recursive call. no biggie.
-        
+
         return super().get(action, 0)
