e00c7e0247b0c306c6f582c353f7c8856433e8ae
Christopher Torrington <ctorrington1@gmail.com>
2024-01-02 20:05:05 +0000

    clean up

--- src/Action.py
+++ src/Action.py
@@ -11,12 +11,12 @@ class Action(Enum):
     """
     Inherit from this class to define actions that can be taken by the agent.
     Actions should be enum member fields of the class.
-    
+
     Generic type for each action available to the Agent.
     """
-    
+
     @classmethod
     def members(cls) -> List[Self]:
         """Return all actions available."""
-        
+
         return list(cls.__members__.values())
