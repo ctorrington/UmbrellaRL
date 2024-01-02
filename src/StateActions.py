39ae0a7dcf37539ede86c1003dc1212c94732d18
Christopher Torrington <ctorrington1@gmail.com>
2024-01-02 19:16:45 +0000

    warning message & clean up

--- src/StateActions.py
+++ src/StateActions.py
@@ -2,31 +2,37 @@
 State Actions type.
 
 Mapping from Actions to a list of potential States from that Action.
-
-Used by the Environment.
 """
 
 from abc import ABC
 from typing import Dict
+from warnings import warn
 
 from src.Action import Action
 from src.StateIndex import StateIndex
 from src.StateProbabilityDistribution import StateProbabilityDistribution
 
-# TODO There is potential for this needing to be calculated dynamically, rather than programmed.
 class StateActions[SI: StateIndex, A: Action](ABC, Dict[A, StateProbabilityDistribution[SI]]):
-    def __getitem__(self,
-                    key: A
-                   ) -> StateProbabilityDistribution[SI]:
+    def __getitem__(
+        self,
+        key: A
+        ) -> StateProbabilityDistribution[SI]:
         """
         Return the State Probability Distribution for the State's accessible 
         following the given Action.
         """
-        
+
+        warn("Using square bracket notation for accessing the StateProbabilityDistribution class is not the preferred method. Consider explicitly using get_state_probability_distribution method available to the StateActions class.")
+
         return self[key]
-    
-    def get_state_probability_distribution(self,
-                                           action: A
-                                          ) -> StateProbabilityDistribution[SI]:
-        """Return the Action Probability Distribution for the given Action."""
+
+    def get_state_probability_distribution(
+        self,
+        action: A
+        ) -> StateProbabilityDistribution[SI]:
+        """
+        Return the State Probability Distribution for the State's accessible 
+        following the given Action.
+        """
+
         return self[action]
