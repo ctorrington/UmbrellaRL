4362e8abb541072d1c1d9a127f3013a1cb2f1203
Christopher Torrington <ctorrington1@gmail.com>
2024-01-07 16:01:42 +0000

    use new state properties

--- src/Agent/Agent.py
+++ src/Agent/Agent.py
@@ -8,6 +8,7 @@ from src.StateSpace import StateSpace
 from src.Environment.Environment import Environment
 from src.Policy.BasePolicy import BasePolicy
 from src.StateIndex import StateIndex
+from src.State import State
 from src.Action import Action
 from src.Agent.AgentService import AgentService
 from src.Agent.History import History
@@ -25,21 +26,21 @@ class Agent[SI: StateIndex, A: Action]:
         environment: Environment[SI, A],
         policy: BasePolicy[SI, A],
         ) -> None:
-        
+
         self.history = History[SI, A]()
-        
+
         self.theta: float = 0.01
-        
+
         self.gamma: float = 0.9
-        
+
         self.environment: Environment[SI, A] = environment
-        
+
         self.policy: BasePolicy[SI, A] = policy
-    
+
     def evaluate_policy(self) -> None:
         """
         Evaluate the policy.
-        
+
         Determine the state-value function for the policy.
         """
 
@@ -49,30 +50,32 @@ class Agent[SI: StateIndex, A: Action]:
 
             delta = 0
 
-            for state in state_space:
+            for state_index in state_space:
 
-                if state_space[state].is_terminal:
+                if state_space[state_index].is_terminal:
 
                     continue
 
-                old_state_value: float = state_space[state].estimated_return
+                old_state_value: float = state_space[state_index].estimated_return
 
                 updated_state_value: float = AgentService.calculate_state_value(
-                    state,
+                    state_index,
                     state_space,
                     self.policy,
                     self.environment,
                     self.gamma
                 )
 
-                self.assign_new_state_estimated_return(state, updated_state_value)
+                state: State[A] = state_space.get_state(state_index)
+
+                state.estimated_return = updated_state_value
 
                 delta = max(delta, (abs(old_state_value - updated_state_value)))
 
             if delta < self.theta:
 
                 self.history.track_state_space(state_space)
-                
+
                 self.history.increment_history_count()
 
                 break
@@ -80,58 +83,46 @@ class Agent[SI: StateIndex, A: Action]:
     def improve_policy(self) -> None:
         """
         Improve the Policy.
-        
+
         Determine optimal actions for each State in the State Space.
         """
-        
+
         state_space: StateSpace[SI, A] = self.environment.get_state_space()
-        
+
         while True:
-            
+
             policy_stable: bool = True
-        
-            for state in state_space:
-                
+
+            for state_index in state_space:
+
                 # TODO create has_actions method
-                if len(state_space[state].actions) == 0:
+                if len(state_space[state_index].actions) == 0:
                     continue
-                
-                old_state_policy: ActionProbabilityDistribution[A] = deepcopy(self.policy.get_action_probability_distribution(state))
-                
+
+                old_state_policy: ActionProbabilityDistribution[A] = deepcopy(self.policy.get_action_probability_distribution(state_index))
+
                 new_greedy_actions: List[A] = AgentService.determine_greedy_actions(
-                    state,
+                    state_index,
                     state_space,
                     self.environment,
                     self.gamma
                 )
-                
+
                 self.policy.set_new_state_policy(
-                    state,
+                    state_index,
                     new_greedy_actions
                 )
-                
-                new_state_policy: ActionProbabilityDistribution[A] = self.policy.get_action_probability_distribution(state)
-                
+
+                new_state_policy: ActionProbabilityDistribution[A] = self.policy.get_action_probability_distribution(state_index)
+
                 if old_state_policy != new_state_policy:
-                    
+
                     policy_stable = False
-                    
+
             if policy_stable:
-                
+
                 break
-            
+
             else:
-                
+
                 self.evaluate_policy()
-                
-    def assign_new_state_estimated_return(
-        self,
-        state: SI,
-        value: float
-        ) -> None:
-        """Assign the new estimated return (value) to the given State."""
-        
-        state_space: StateSpace[SI, A] = self.environment.get_state_space()
-        
-        # TODO State needs a set method for estimated return.
-        state_space[state].estimated_return = value
