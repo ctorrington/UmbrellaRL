cb7191879ca8dc784ea877b7ff57929910320642
Christopher Torrington <ctorrington1@gmail.com>
2024-01-07 15:25:26 +0000

    add property accessors

--- src/State.py
+++ src/State.py
@@ -16,9 +16,89 @@ class State[A: Action]:
     """
 
     def __init__(self):
-        self.actions: List[A] = []
-        self.estimated_return: float = 0.0
-        self.counter: int = 0
-        self.reward: float = 0
-        self.is_current: bool = False
-        self.is_terminal: bool = False
+
+        self._actions: List[A] = []
+
+        self._estimated_return: float = 0.0
+
+        self._counter: int = 0
+
+        self._reward: float = 0
+
+        self._is_current: bool = False
+
+        self._is_terminal: bool = False
+
+    @property
+    def actions(self) -> List[A]:
+        
+        return self.actions
+
+    @actions.setter
+    def actions(
+        self,
+        actions: List[A]
+        ) -> None:
+        
+        self._actions = actions
+
+    @property
+    def estimated_return(self) -> float:
+        
+        return self._estimated_return
+
+    @estimated_return.setter
+    def estimated_return(
+        self,
+        estimated_return: float
+    ) -> None:
+        
+        self._estimated_return = estimated_return
+
+    @property
+    def counter(self) -> int:
+        
+        return self._counter
+    
+    def increment_counter(self) -> None:
+        
+        self._counter += 1
+
+    @property
+    def reward(self) -> float:
+        
+        return self._reward
+
+    @reward.setter
+    def reward(
+        self,
+        reward: float
+    ) -> None:
+        
+        self._reward = reward
+
+    @property
+    def is_current(self) -> bool:
+        
+        return self._is_current
+
+    @is_current.setter
+    def is_current(
+        self,
+        is_current: bool
+    ) -> None:
+        
+        self._is_current = is_current
+
+    @property
+    def is_terminal(self) -> bool:
+        
+        return self._is_terminal
+
+    @is_terminal.setter
+    def is_terminal(
+        self,
+        is_terminal: bool
+    ) -> None:
+        
+        self._is_terminal = is_terminal
