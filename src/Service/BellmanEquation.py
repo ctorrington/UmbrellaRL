9eb70f3752ec786219e247a33732e568dd2518c4
Christopher Torrington <ctorrington1@gmail.com>
2024-01-07 15:42:58 +0000

    use new state scoped functions

--- src/Service/BellmanEquation.py
+++ src/Service/BellmanEquation.py
@@ -1,8 +1,11 @@
 """Bellman Equation service class."""
 
+from typing import List
+
 from src.StateIndex import StateIndex
 from src.Action import Action
 from src.StateSpace import StateSpace
+from src.State import State
 from src.Environment.Environment import Environment
 from src.StateProbabilityDistribution import StateProbabilityDistribution
 from src.ActionProbabilityDistribution import ActionProbabilityDistribution
@@ -13,25 +16,29 @@ class BellmanEquation[SI: StateIndex, A: Action]():
     
     @classmethod
     def calculate_state_value(cls,
-                              state: SI,
+                              state_index: SI,
                               state_space: StateSpace[SI, A],
                               policy: BasePolicy[SI, A],
                               environment: Environment[SI,A],
                               gamma: float
                              ) -> float:
         """Calculate the state value """
-        
+
         state_value: float = 0
+
+        state_action_probability_distribution: ActionProbabilityDistribution[A] = policy.get_action_probability_distribution(state_index)
+
+        state: State[A] = state_space.get_state(state_index)
+
+        state_actions: List[A] = state.actions
         
-        state_action_probability_distribution: ActionProbabilityDistribution[A] = policy.get_action_probability_distribution(state)
-        
-        for action in state_space[state].actions:
+        for action in state_actions:
             
             # TODO get method for below
             action_probability: float = state_action_probability_distribution[action]
             
             update_value: float = cls.calculate_update_value(
-                state,
+                state_index,
                 action,
                 state_space,
                 environment,
@@ -51,30 +58,32 @@ class BellmanEquation[SI: StateIndex, A: Action]():
                                gamma: float
                               ) -> float:
         """Calculate the update to the Bellman Equation."""
-        
+
         update_value: float = 0
-        
+
         next_states: StateProbabilityDistribution[SI] = environment.get_next_states(
             state,
             action
         )
-        
-        for next_state in next_states:
-            
+
+        for next_state_index in next_states:
+
             next_state_probability: float = environment.get_state_transition_probability(
                 state,
                 action,
-                next_state
+                next_state_index
             )
-            
-            next_state_reward: float = state_space.get_reward(next_state)
-            
-            next_state_value: float = state_space.get_estimated_return(next_state)
-            
+
+            next_state: State[A] = state_space.get_state(next_state_index)
+
+            next_state_reward: float = next_state.reward
+
+            next_state_value: float = next_state.estimated_return
+
             update_value += next_state_probability * (next_state_reward + (gamma * next_state_value))
-            
+
         return update_value
-    
+
     @classmethod
     def calculate_state_action_value(cls,
                                      state: SI,
@@ -85,12 +94,12 @@ class BellmanEquation[SI: StateIndex, A: Action]():
                                      ) -> float:
         """
         Calculate the state action-value.
-        
+
         From my understanding, the update value to the state value function is 
         equivalent to the state-action value. When greedy policy simply chooses 
         the maximising state-action (update) value.
         """
-        
+
         state_action_value: float = cls.calculate_update_value(
             state,
             action,
@@ -98,5 +107,5 @@ class BellmanEquation[SI: StateIndex, A: Action]():
             environment,
             gamma
             )
-        
+
         return state_action_value
