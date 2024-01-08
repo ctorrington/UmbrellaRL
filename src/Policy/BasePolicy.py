80bb28fa5fa21a0cff96e40273d0b10dc4ff42e7
Christopher Torrington <ctorrington1@gmail.com>
2024-01-08 17:30:19 +0000

    use more accurate variable names

--- src/Policy/BasePolicy.py
+++ src/Policy/BasePolicy.py
@@ -17,10 +17,13 @@ from src.Action import Action
 # TODO Relook at the Policies, they need to be tidied up.
 
 class BasePolicy[SI: StateIndex, A: Action](ABC, Dict[SI, ActionProbabilityDistribution[A]]):
+
     @abstractmethod
-    def choose_action(self,
-                      state: SI
-                     ) -> A:
+    def choose_action(
+        self,
+        state_index: SI
+    ) -> A:
+
         """
         Policy selects an action for the given State according to the State's
         Action Probability Distribution & the goal of the Policy.
@@ -28,47 +31,55 @@ class BasePolicy[SI: StateIndex, A: Action](ABC, Dict[SI, ActionProbabilityDistr
  
         pass
 
-    @abstractmethod
-    def get_action_probability_distribution(self,
-                                            state: SI
-                                           ) -> ActionProbabilityDistribution[A]:
+    def get_action_probability_distribution(
+        self,
+        state_index: SI
+    ) -> ActionProbabilityDistribution[A]:
+
         """Return the Action Probability Distribution for the given state."""
         
-        return ActionProbabilityDistribution(self[state])
-    
-    def get_greedy_actions(self,
-                           state: SI
-                          ) -> List[A]:
+        return ActionProbabilityDistribution(self[state_index])
+
+    def get_greedy_actions(
+        self,
+        state_index: SI
+    ) -> List[A]:
+
         """Return the Actions with the highest probability of being chosen."""
-        
+
         greedy_actions: List[A] = []
-        
-        greedy_value = max(self.get_action_probability_distribution(state).values())
-        
-        action_prob_dist = self[state]
+
+        greedy_value = max(self.get_action_probability_distribution(state_index).values())
+
+        # TODO type of self.get_action_probability_distribution is not correct type (ActionProbabilityDistribution).
+        action_prob_dist: ActionProbabilityDistribution[A] = ActionProbabilityDistribution(self.get_action_probability_distribution(state_index))
         
         for action in action_prob_dist:
-            
-            # TODO idk why this is needed.
-            if ActionProbabilityDistribution(action_prob_dist).get_action_probability(action) >= greedy_value:
-                
+
+            if action_prob_dist.get_action_probability(action) >= greedy_value:
+
                 greedy_actions.append(action)
-        
+
         return greedy_actions
-   
+
     # TODO should not be a list (this is equiprobable)
-    def set_new_state_policy(self,
-                             state: SI,
-                             new_actions: List[A]
-                            ) -> None:
+    def set_new_state_policy(
+        self,
+        state: SI,
+        new_actions: List[A]
+    ) -> None:
         """Set a new Policy for given State."""
-        
+
         state_action_probability_distribution: ActionProbabilityDistribution[A] = self.get_action_probability_distribution(state)
-        
+
         new_action_probability: float = 1 / len(new_actions)
-        
+
         for action in state_action_probability_distribution:
+
             if action in new_actions:
+
                 self[state][action] = new_action_probability
+
             else:
+
                 self[state][action] = 0
