738bb335d37a96bb96c94b6afb893ec0f8a66393
Christopher Torrington <ctorrington1@gmail.com>
2024-07-09 23:51:33 +0100

    add init method abstraction

--- Solutions/GridWorld/StateSpace.py
+++ Solutions/GridWorld/StateSpace.py
@@ -17,25 +17,27 @@ class GridWorldStateSpace(StateSpace[GridWorldStateIndex, GridWorldAction]):
         number_of_columns: int,
         terminal_states: Optional[List[GridWorldStateIndex]] = None
     ) -> None:
-        self.terminal_states = terminal_states
-        # Set terminal states if they are not provided.
-        if terminal_states is None:
-            self.terminal_states = [
-                (0, 0),
-                (number_of_rows - 1, number_of_columns - 1)
-            ]
         self.number_of_rows = number_of_rows
         self.number_of_columns = number_of_columns
+        self.terminal_states = terminal_states or [
+            (0, 0),
+            (number_of_rows - 1, number_of_columns - 1)
+        ]
+        
+        self._initialise_states()
+        self._initialise_terminal_states()
 
-        for row in range(number_of_rows):
-            for column in range(number_of_columns):
-
+    def _initialise_states(self) -> None:
+        """Initialise all states."""
+        for row in range(self.number_of_rows):
+            for column in range(self.number_of_columns):
                 grid_world_state: GridWorldState = GridWorldState(row, column)
                 grid_world_state.actions = GridWorldAction.members()
                 self[(row, column)] = grid_world_state
 
+    def _initialise_terminal_states(self) -> None:
+        """Initialise & set terminal state values."""
         for state_index in self.terminal_states:
-
             state = self.get_state(state_index)
             state.is_terminal = True
             state.reward = 1.0
