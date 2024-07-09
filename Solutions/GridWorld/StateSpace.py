1803bdea7891e873d574e5ad0ea267da6f41e4eb
Christopher Torrington <ctorrington1@gmail.com>
2024-07-09 23:04:37 +0100

    fix formatting

--- Solutions/GridWorld/StateSpace.py
+++ Solutions/GridWorld/StateSpace.py
@@ -15,32 +15,25 @@ class GridWorldStateSpace(StateSpace[GridWorldStateIndex, GridWorldAction]):
         self,
         number_of_rows: int,
         number_of_columns: int
-        ) -> None:
+    ) -> None:
 
         # TODO will probably have some config file for these terminal states. 
         terminal_states: List[GridWorldStateIndex] = [
             (0, 0),
             (number_of_rows - 1, number_of_columns - 1)
             ]
-
         self.number_of_rows = number_of_rows
-
         self.number_of_columns = number_of_columns
 
         for row in range(number_of_rows):
-
             for column in range(number_of_columns):
 
                 grid_world_state: GridWorldState = GridWorldState(row, column)
-
                 grid_world_state.actions = GridWorldAction.members()
-
                 self[(row, column)] = grid_world_state
 
         for state_index in terminal_states:
 
             state = self.get_state(state_index)
-
             state.is_terminal = True
-
             state.reward = 1.0
