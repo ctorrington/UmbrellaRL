f2cc4ffd3fb825778ebe3018c168b45784838047
Christopher Torrington <ctorrington1@gmail.com>
2024-07-09 22:19:21 +0100

    fix formatting

--- core/Environment/Environment.py
+++ core/Environment/Environment.py
@@ -18,23 +18,20 @@ class Environment[SI: StateIndex, A: Action](ABC, Dict[SI, StateActions[SI, A]])
     def __init__(
         self,
         state_space: StateSpace[SI, A]
-        ) -> None:
-
+    ) -> None:
         self.state_space: StateSpace[SI, A] = state_space
 
     def get_next_states(
         self,
         current_state_index: SI,
         action: A
-        ) -> StateProbabilityDistribution[SI]:
-
+    ) -> StateProbabilityDistribution[SI]:
         return self.get_state_actions(current_state_index).get_state_probability_distribution(action)
 
     def get_state_actions(
         self,
         state_index: SI
-        ) -> StateActions[SI, A]:
-        
+    ) -> StateActions[SI, A]:
         return StateActions(self[state_index])
 
     def get_state_transition_probability(
@@ -42,20 +39,17 @@ class Environment[SI: StateIndex, A: Action](ABC, Dict[SI, StateActions[SI, A]])
         current_state_index: SI,
         action: A,
         next_state_index: SI
-        ) -> float:
+    ) -> float:
         """
         Return the probability of transitioning from one state to another
         given an action.
         """
-
         return self.get_state_actions(current_state_index).get_state_probability_distribution(action).get_state_probability(next_state_index)
 
-    def number_of_states(self):
+    def number_of_states(self) -> int:
         """Return the number of State's in the State Space."""
-
         return len(self)
 
     def get_state_space(self) -> StateSpace[SI, A]:
         """Return the State Space."""
-
         return self.state_space
