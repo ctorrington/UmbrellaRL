9b2dfc649be8cf7346e620f2097f9dc5cfbeb61d
Christopher Torrington <ctorrington1@gmail.com>
2024-07-09 22:25:48 +0100

    fix formatting

--- core/Agent/Agent.py
+++ core/Agent/Agent.py
@@ -27,16 +27,13 @@ class Agent[SI: StateIndex, A: Action]:
         policy: BasePolicy[SI, A],
         theta: float = 0.01,
         gamma: float = 0.9
-        ) -> None:
+    ) -> None:
+        # TODO Doc String.
 
         self.history = History[SI, A]()
-
         self.theta: float = theta
-
         self.gamma: float = gamma
-
         self.environment: Environment[SI, A] = environment
-
         self.policy: BasePolicy[SI, A] = policy
 
     def evaluate_policy(self) -> None:
@@ -49,19 +46,14 @@ class Agent[SI: StateIndex, A: Action]:
         state_space: StateSpace[SI, A] = self.environment.get_state_space()
 
         while True:
-
             delta = 0
-
             for state_index in state_space:
 
                 state: State[A] = state_space.get_state(state_index)
-
                 if state.is_terminal:
-
                     continue
 
                 old_state_value: float = state.estimated_return
-
                 updated_state_value: float = AgentService.calculate_state_value(
                     state_index,
                     state_space,
@@ -69,17 +61,12 @@ class Agent[SI: StateIndex, A: Action]:
                     self.environment,
                     self.gamma
                 )
-
                 state.estimated_return = updated_state_value
-
                 delta = max(delta, (abs(old_state_value - updated_state_value)))
 
             if delta < self.theta:
-
                 self.history.track_state_space(state_space)
-
                 self.history.increment_history_count()
-
                 break
 
     def improve_policy(self) -> None:
@@ -94,39 +81,28 @@ class Agent[SI: StateIndex, A: Action]:
         while True:
 
             policy_stable: bool = True
-
             for state_index in state_space:
                 
                 state: State[A] = state_space.get_state(state_index)
-
                 if not state.has_actions():
-
                     continue
 
                 old_state_policy: ActionProbabilityDistribution[A] = deepcopy(self.policy.get_action_probability_distribution(state_index))
-
                 new_greedy_actions: List[A] = AgentService.determine_greedy_actions(
                     state_index,
                     state_space,
                     self.environment,
                     self.gamma
                 )
-
                 self.policy.set_new_state_policy(
                     state_index,
                     new_greedy_actions
                 )
-
                 new_state_policy: ActionProbabilityDistribution[A] = self.policy.get_action_probability_distribution(state_index)
-
                 if old_state_policy != new_state_policy:
-
                     policy_stable = False
 
             if policy_stable:
-
                 break
-
             else:
-
                 self.evaluate_policy()
--- core/Policy/EquiprobablePolicy.py
+++ core/Policy/EquiprobablePolicy.py
@@ -19,35 +19,31 @@ class EquiprobablePolicy[SI: StateIndex, A: Action](BasePolicy[SI, A]):
         self,
         state_space: StateSpace[SI, A]
     ) -> None:
-        
+        # TODO Doc string.
+
         self.state_space: StateSpace[SI, A] = state_space
-        
+
         for state in self.state_space:
-            
+
             # TODO Below needed for assigning action probability in next loop.
                 # Figure out how to assign a type here. figure out default dic
             self[state] = {}
-            
             # TODO division by zero risk.
             action_probability: float = 1/ len(self.state_space[state].actions)
-            
+
             for action in state_space[state].actions:
-                
                 self[state][action] = action_probability
-            
+
     def choose_action(
         self,
         state_index: SI
     ) -> A:
         """Choose an action based on the Action Probability Distribution."""
-
         actions: List[A] = list(self[state_index].keys())
-        
         return random.choice(actions)
 
     def get_action_probability_distribution(
         self,
         state_index: SI
     ) -> ActionProbabilityDistribution[A]:
-
         return self[state_index]
