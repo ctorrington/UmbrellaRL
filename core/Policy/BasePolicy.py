f2fc7ab13e3a40bf1315936198cb0dac62a2c9fe
Christopher Torrington <ctorrington1@gmail.com>
2024-07-09 22:21:20 +0100

    fix formatting

--- core/Policy/BasePolicy.py
+++ core/Policy/BasePolicy.py
@@ -23,32 +23,26 @@ class BasePolicy[SI: StateIndex, A: Action](ABC, Dict[SI, ActionProbabilityDistr
         self,
         state_index: SI
     ) -> A:
-
         """
         Policy selects an action for the given State according to the State's
         Action Probability Distribution & the goal of the Policy.
         """
- 
         pass
 
     def get_action_probability_distribution(
         self,
         state_index: SI
     ) -> ActionProbabilityDistribution[A]:
-
         """Return the Action Probability Distribution for the given state."""
-        
         return ActionProbabilityDistribution(self[state_index])
 
     def get_greedy_actions(
         self,
         state_index: SI
     ) -> List[A]:
-
         """Return the Actions with the highest probability of being chosen."""
 
         greedy_actions: List[A] = []
-
         greedy_value = max(self.get_action_probability_distribution(state_index).values())
 
         # TODO type of self.get_action_probability_distribution is not correct type (ActionProbabilityDistribution).
@@ -57,7 +51,6 @@ class BasePolicy[SI: StateIndex, A: Action](ABC, Dict[SI, ActionProbabilityDistr
         for action in action_prob_dist:
 
             if action_prob_dist.get_action_probability(action) >= greedy_value:
-
                 greedy_actions.append(action)
 
         return greedy_actions
@@ -71,15 +64,11 @@ class BasePolicy[SI: StateIndex, A: Action](ABC, Dict[SI, ActionProbabilityDistr
         """Set a new Policy for given State."""
 
         state_action_probability_distribution: ActionProbabilityDistribution[A] = self.get_action_probability_distribution(state)
-
         new_action_probability: float = 1 / len(new_actions)
 
         for action in state_action_probability_distribution:
 
             if action in new_actions:
-
                 self[state][action] = new_action_probability
-
             else:
-
                 self[state][action] = 0
