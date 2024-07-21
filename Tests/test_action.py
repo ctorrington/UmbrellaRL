735abbd82bb715883b5ef735657cef2d912cf34e
Christopher Torrington <ctorrington1@gmail.com>
2024-07-21 15:00:14 +0100

    Add unit tests for grid world action

---
+++ Tests/test_action.py
@@ -0,0 +1,20 @@
+import unittest
+
+from Solutions.GridWorld.Action import GridWorldAction
+
+class TestGridWorldAction(unittest.TestCase):
+    def test_attributes(self):
+        """Test that GridWorldAction has the correct attributes."""
+        self.assertEqual(GridWorldAction.UP.value, "up")
+        self.assertEqual(GridWorldAction.DOWN.value, "down")
+        self.assertEqual(GridWorldAction.LEFT.value, "left")
+        self.assertEqual(GridWorldAction.RIGHT.value, "right")
+
+    def test_members(self):
+        """Test that the members method returns all actions."""
+        expected_members = ["up", "down", "left", "right"]
+        actual_members = [member.value for member in GridWorldAction.members()]
+        self.assertListEqual(sorted(expected_members), sorted(actual_members))
+
+if __name__ == "__main__":
+    unittest.main()
