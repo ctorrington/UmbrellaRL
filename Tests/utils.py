6ed713732390b3c7597999c5deaba3c6ad54da7b
Christopher Torrington <ctorrington1@gmail.com>
2024-08-01 17:54:01 +0100

    Add class name to unit tests

--- Tests/utils.py
+++ Tests/utils.py
@@ -11,13 +11,19 @@ logger: Logger = LoggerManager().get_logger('UnitTestLogger')
 
 def timed(func: Callable[..., Any]) -> Callable[..., Any]:
     """Decorator method to measure unit test execution time."""
+    
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
+        
+        # Measure execution time.
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
         run_time = end_time - start_time
-        logger.debug(f"{func.__name__} ran in {run_time:.6f}s.")
+
+        # Attempt class name.
+        class_name = args[0].__class__.__name__ if args else 'oh no'
+        logger.debug(f"{class_name}.{func.__name__} ran in {run_time:.6f}s.")
 
         return result
 
