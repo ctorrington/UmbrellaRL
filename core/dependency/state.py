437d40a5ddec6cd7fbee91b226d996d4085ef335
Christopher Torrington <ctorrington1@gmail.com>
2024-08-03 14:27:06 +0100

    Remove getters & setters & add parameter validation

--- core/dependency/state.py
+++ core/dependency/state.py
@@ -6,7 +6,7 @@ A State's estimated return is synonymous a State's value.
 """
 import logging
 
-from typing import List
+from typing import List, Any
 
 from core.dependency.action import Action
 from log.ilogger import ILogger
@@ -29,89 +29,53 @@ class State[A: Action]:
         """Initialisation for an individual State.
 
         Args:
-            state_action_list (List[A]): Agent Actions available within the 
+            action_list (List[A]): Agent Actions available within the 
             State. The type of the list is inherited from the 
             core.dependency.action class.
             estimated_return (float): Float value for the estimated return of 
             the State.
             reward (float): Float value for the reward of the Agent reaching 
             the State.
+            is_terminal (bool): Boolean value for if the State is a Terminal 
+            State
         """
+        self._validate_parameters(action_list=action_list, estimated_return=estimated_return, reward=reward, is_terminal=is_terminal, logger=logger)
         # Assigned properties.
         self._logger: logging.Logger = logger.get_logger(self.__class__.__name__)
-        self._actions: List[A] = action_list
-        self._estimated_return: float = estimated_return
-        self._reward: float = reward
-        self._is_terminal: bool = is_terminal
+        self.actions: List[A] = action_list
+        self.estimated_return: float = estimated_return
+        self.reward: float = reward
+        self.is_terminal: bool = is_terminal
 
-        self._is_current: bool = False
-        self._counter: int = 0
+        self.is_current: bool = False
+        self.counter: int = 0
 
         self._logger.info(
-            f"State initialised with actions: {action_list}, estimated return: {estimated_return}, reward: {reward}, current state: {self._is_current}, terminal state: {self._is_terminal}, counter: {self._counter}."
+            f"State initialised with actions: {self.actions}, estimated return: {self.estimated_return}, reward: {self.reward}, current state: {self.is_current}, terminal state: {self.is_terminal}, counter: {self.counter}."
         )
 
-    @property
-    def actions(self) -> List[A]:
-        return self._actions
-
-    @actions.setter
-    def actions(
+    def _validate_parameters(
         self,
-        actions: List[A]
+        action_list: Any,
+        estimated_return: Any,
+        reward: Any,
+        is_terminal: Any,
+        logger: Any
     ) -> None:
-        self._actions = actions
+        if not isinstance(action_list, list) or not all(isinstance(action, Action) for action in action_list):
+            raise TypeError(f"action_list must be list of type Action instances.")
+        if not isinstance(estimated_return, float):
+            raise TypeError(f"estimated_return must be of type float.")
+        if not isinstance(reward, float):
+            raise TypeError(f"reward must be of type float.")
+        if not isinstance(is_terminal, bool):
+            raise TypeError(f"is_terminal must be of type bool.")
+        if not isinstance(logger, ILogger):
+            raise TypeError(f"logger must be of type ILogger.")
 
     def has_actions(self) -> bool:
-        return bool(self._actions)
+        return bool(self.actions)
 
-    @property
-    def estimated_return(self) -> float:
-        return self._estimated_return
-
-    @estimated_return.setter
-    def estimated_return(
-        self,
-        estimated_return: float
-    ) -> None:
-        self._estimated_return = estimated_return
-
-    @property
-    def counter(self) -> int:
-        return self._counter
-    
     def increment_counter(self) -> None:
-        self._counter += 1
-
-    @property
-    def reward(self) -> float:
-        return self._reward
-
-    @reward.setter
-    def reward(
-        self,
-        reward: float
-    ) -> None:
-        self._reward = reward
-
-    @property
-    def is_current(self) -> bool:
-        return self._is_current
-
-    @is_current.setter
-    def is_current(
-        self,
-        is_current: bool
-    ) -> None:
-        self._is_current = is_current
-
-    @property
-    def is_terminal(self) -> bool:
-        return self._is_terminal
-
-    @is_terminal.setter
-    def is_terminal(
-        self,
-        is_terminal: bool
-    ) -> None:
-        self._is_terminal = is_terminal
+        self._logger.info(f"Incrementing State.counter from {self.counter} to {self.counter + 1}.")
+        self.counter += 1
